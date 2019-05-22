33e0812ec8218924333c337ef5a2b234e1a2de9b
Travis CI User <travis@example.org>
2019-05-22 15:38:58 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["backtrace"] = {"name":"","dirs":[{"name":"backtrace","files":["libunwind.rs","mod.rs"]},{"name":"symbolize","files":["libbacktrace.rs","mod.rs"]}],"files":["capture.rs","dylib.rs","lib.rs","types.rs"]};
+sourcesIndex["backtrace"] = {"name":"","dirs":[{"name":"backtrace","files":["libunwind.rs","mod.rs"]},{"name":"symbolize","files":["libbacktrace.rs","mod.rs"]}],"files":["capture.rs","lib.rs","types.rs"]};
 sourcesIndex["backtrace_sys"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["failure"] = {"name":"","dirs":[{"name":"backtrace","files":["internal.rs","mod.rs"]},{"name":"error","files":["error_impl.rs","mod.rs"]}],"files":["as_fail.rs","box_std.rs","compat.rs","context.rs","error_message.rs","lib.rs","macros.rs","result_ext.rs","sync_failure.rs"]};
@@ -34,7 +34,7 @@ sourcesIndex["rand_xorshift"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["rustc_demangle"] = {"name":"","files":["legacy.rs","lib.rs","v0.rs"]};
 sourcesIndex["serde"] = {"name":"","dirs":[{"name":"de","files":["from_primitive.rs","ignored_any.rs","impls.rs","mod.rs","utf8.rs","value.rs"]},{"name":"private","files":["de.rs","macros.rs","mod.rs","ser.rs"]},{"name":"ser","files":["impls.rs","impossible.rs","mod.rs"]}],"files":["export.rs","integer128.rs","lib.rs","macros.rs"]};
 sourcesIndex["siphasher"] = {"name":"","files":["lib.rs","sip.rs","sip128.rs"]};
-sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["gen_helper.rs"]}],"files":["attr.rs","buffer.rs","data.rs","derive.rs","error.rs","expr.rs","generics.rs","lib.rs","lifetime.rs","lit.rs","mac.rs","macros.rs","op.rs","parse_quote.rs","parsers.rs","path.rs","punctuated.rs","spanned.rs","synom.rs","token.rs","tt.rs","ty.rs"]};
+sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["gen_helper.rs","visit.rs"]}],"files":["attr.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","error.rs","export.rs","expr.rs","ext.rs","file.rs","generics.rs","group.rs","ident.rs","item.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","tt.rs","ty.rs"]};
 sourcesIndex["synstructure"] = {"name":"","files":["lib.rs","macros.rs"]};
 sourcesIndex["type_info"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["type_info_derive"] = {"name":"","files":["lib.rs"]};
